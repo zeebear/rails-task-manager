d7165139f4b07bf486bef97db8033b2bf1836873
Claire Fahy <zeebear21@gmail.com>
2019-02-12 15:14:26 -0300

    add ability to add task, delete task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: "new"
+  post "tasks", to: "tasks#create"
   get "tasks/:id", to: "tasks#show", as: "show"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: "delete"
 end
