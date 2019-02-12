6a9fa4736f1699618afdee6d2d02a454efb5a2db
Claire Fahy <zeebear21@gmail.com>
2019-02-12 15:33:52 -0300

    edit task

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new", as: "new"
   post "tasks", to: "tasks#create"
-  get "tasks/:id", to: "tasks#show", as: "show"
+  get "tasks/:id", to: "tasks#show", as: "task"
   get "tasks/:id/edit", to: "tasks#edit", as: "edit"
   patch "tasks/:id", to: "tasks#update"
   delete "tasks/:id", to: "tasks#destroy", as: "delete"
